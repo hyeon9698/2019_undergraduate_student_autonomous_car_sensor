662307abfec6dcb87db34b582b8a2b55e38af745
hyeon9698 <41141851+hyeon9698@users.noreply.github.com>
2019-02-06 22:23:16 +0900

    Add files via upload

---
+++ Matlab_Code/2019_01_17_face_detection/face_detection_viola_jones_02.m
@@ -0,0 +1,14 @@
+% Test face detection code
+close all;
+clear;
+clc;
+A = imread ('face.png', 'PNG');
+Img = double (rgb2gray(A));
+Face = FaceDetect('haarcascade_frontalface_alt2.xml',Img);
+Rectangle = [Face(1) Face(2); Face(1)+Face(3) Face(2); Face(1)+Face(3) Face(2)+Face(4); Face(1)  Face(2)+Face(4); Face(1) Face(2)];
+figure(1);
+imshow (A);
+truesize;
+hold on;
+plot (Rectangle(:,1), Rectangle(:,2), 'g');
+hold off;
